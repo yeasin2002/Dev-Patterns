8e65813aa9869d176f96c69878cf219195d004f1
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-03-27 16:46:08 +0600

    feat: created initial homepage components

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/principles/solid-principles.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprinciples%2Fsolid-principles.mdx&astroContentModuleFlag=true")],
 ["src/content/patterns/singleton-pattern.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpatterns%2Fsingleton-pattern.mdx&astroContentModuleFlag=true")]]);
 		
