3f6361cca23b75a7b3f2ef024f496913b32d149f
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-03-28 15:18:35 +0600

    refactor: moved svg into icon components

--- src/assets/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import Arrow from "./arrow.svg";
-import File from "./file.svg";
-import Menu from "./menu.svg";
-import PlusMini from "./plus-mini.svg";
-
-export { Arrow, File, Menu, PlusMini };
---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+import Arrow from "./arrow.astro";
+import Copy from "./copy.astro";
+import File from "./file.astro";
+import Menu from "./menu.astro";
+import PlusMini from "./plus-mini.astro";
+
+export { Arrow, Copy, File, Menu, PlusMini };
