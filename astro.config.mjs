3b922558dc7c5d2eb3e46c4a1c1dc0b15ce91090
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-03-29 03:41:25 +0600

    fix: hom-page and collection page design issues including fonts, size and spacing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import UnFonts from "unplugin-fonts/vite";
 
 export default defineConfig({
   integrations: [, mdx()],
@@ -12,18 +11,6 @@ export default defineConfig({
     svg: { mode: "sprite" },
   },
   vite: {
-    plugins: [
-      tailwindcss(),
-      UnFonts({
-        google: {
-          families: [
-            {
-              name: "Noto Serif Bengali",
-              styles: "wght@300;400;500;700",
-            },
-          ],
-        },
-      }),
-    ],
+    plugins: [tailwindcss()],
   },
 });
