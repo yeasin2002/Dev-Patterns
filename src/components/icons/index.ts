9c45369d99da39eeab28dd2ba254dece5272a5b9
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-03-28 23:58:54 +0600

    feat: design collection and articles page with proper data

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,8 +2,22 @@ import Arrow from "./arrow.astro";
 import Calender from "./calender.astro";
 import Copy from "./copy.astro";
 import File from "./file.astro";
+import Github from "./github-logo.astro";
 import Menu from "./menu.astro";
 import PlusMini from "./plus-mini.astro";
 import User from "./user.astro";
+import Linkedin from "./Linkedin.astro";
+import Twitter from "./Twitter.astro";
 
-export { Arrow, Calender, Copy, File, Menu, PlusMini, User };
+export {
+  Arrow,
+  Calender,
+  Copy,
+  File,
+  Github,
+  Menu,
+  PlusMini,
+  User,
+  Linkedin,
+  Twitter,
+};
---
+++ src/data/index.ts
@@ -0,0 +1,4 @@
+export * from "./collections-page.data";
+export * from "./features";
+export * from "./testimonials";
+
