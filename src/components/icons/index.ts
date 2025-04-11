df9cb3393a196c6f60de58d907a9a865838be4ec
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-04-12 03:10:21 +0600

    fix: footer icons proper url

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,23 +1,25 @@
+import Linkedin from "./Linkedin.astro";
+import Twitter from "./Twitter.astro";
 import Arrow from "./arrow.astro";
 import Calender from "./calender.astro";
 import Copy from "./copy.astro";
+import Facebook from "./facebook.astro";
 import File from "./file.astro";
 import Github from "./github-logo.astro";
 import Menu from "./menu.astro";
 import PlusMini from "./plus-mini.astro";
 import User from "./user.astro";
-import Linkedin from "./Linkedin.astro";
-import Twitter from "./Twitter.astro";
 
 export {
   Arrow,
   Calender,
   Copy,
+  Facebook,
   File,
   Github,
+  Linkedin,
   Menu,
   PlusMini,
-  User,
-  Linkedin,
   Twitter,
+  User,
 };
